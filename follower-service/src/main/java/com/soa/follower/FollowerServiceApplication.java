86e913851328ac442c541aa6c6791a2f7912c558
gagicanja <gagic.ra165.2022@uns.ac.rs>
2026-05-18 03:11:49 +0200

    feat: adding controller and services

---
+++ follower-service/src/main/java/com/soa/follower/FollowerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.soa.follower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FollowerServiceApplication.class, args);
+    }
+}
